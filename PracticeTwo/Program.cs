c7332d793dd6f4f37205a9369be1ead5cee1562d
Grace Carola T <gracetorrico1@upb.edu>
2023-05-02 20:26:28 -0400

    P3-001 Logs configured for Dev and QA environment

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -4,21 +4,6 @@ using UPB.PracticeTwo.Middlewares;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args); //Servidor
-//creating the logger and setting up sinks, filters and properties
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File("logs/xxx.log", rollingInterval: RollingInterval.Day)
-    .CreateBootstrapLogger();
-
-
-//after create the builder - UseSerilog
-builder.Host.UseSerilog();
-
-// Add services to the container.(Servicios)
-builder.Services.AddTransient<PatientManager>();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 var configurationBuilder = new ConfigurationBuilder()
     .SetBasePath(builder.Environment.ContentRootPath)
@@ -28,6 +13,31 @@ var configurationBuilder = new ConfigurationBuilder()
 
 IConfiguration Configuration = configurationBuilder.Build(); 
 string siteTitle = Configuration.GetSection("Title").Value;
+var environment = Configuration.GetValue<string>("Environment");
+
+//creating the logger and setting up sinks, filters and properties
+LoggerConfiguration loggerConfiguration;
+if (environment == "Development")
+{
+    loggerConfiguration = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("logs/DEV.log", rollingInterval: RollingInterval.Day);
+}
+else
+{
+    loggerConfiguration = new LoggerConfiguration()
+        .WriteTo.File("logs/QA.log", rollingInterval: RollingInterval.Day);
+}
+
+Log.Logger = loggerConfiguration.CreateBootstrapLogger();
+
+//after create the builder - UseSerilog
+builder.Host.UseSerilog();
+
+// Add services to the container.(Servicios)
+builder.Services.AddTransient<PatientManager>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
 {
@@ -45,13 +55,6 @@ var app = builder.Build();
 app.UseGlobalExceptionHandler();
 app.UseSwagger();
 app.UseSwaggerUI();
-/*if (app.Environment.IsDevelopment())
-{
-    
-}*/
 
-//app.UseHttpsRedirection();
-//app.UseRouting();
-//app.UseAuthorization();
 app.MapControllers();
 app.Run();
